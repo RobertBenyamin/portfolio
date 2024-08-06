4358fd0e1233097c8d309f23814f28ecf9ad75d9
RobertBenyamin <robertbenyamino@gmail.com>
2024-08-06 15:16:48 +0700

    feat: update Navbar styling and links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,23 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-900 text-white p-4 sticky top-0">
+    <nav className="bg-gray-800 text-white p-8 sticky top-0">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-lg font-bold">Robert Benyamin</div>
-        <div>
-          <a href="#about" className="mx-2">
+        <div className="text-xl font-bold">Robert Benyamin</div>
+        <div className="space-x-8">
+          <a href="#about" className="mx-2 text-lg">
             About
           </a>
-          <a href="#portfolio" className="mx-2">
+          <a href="#skills" className="mx-2 text-lg">
+            Skills
+          </a>
+          <a href="#portfolio" className="mx-2 text-lg">
             Portfolio
           </a>
-          <a href="#contact" className="mx-2">
+          <a href="#experience" className="mx-2 text-lg">
+            Experience
+          </a>
+          <a href="#contact" className="mx-2 text-lg">
             Contact
           </a>
         </div>
